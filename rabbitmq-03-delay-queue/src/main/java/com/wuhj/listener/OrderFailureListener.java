bea35e34c99ec30ebf08f80b376dfd34fa4e0bdc
吴华俊 <wuhuajun@htgd.com.cn>
2020-12-24 10:41:38 +0800

    rabbitmq整合springboot 通过ttl和死信队列实现延时队列

--- rabbitmq-03-delay-queue/src/main/java/com/wuhj/listener/OrderFailureListener.java
+++ rabbitmq-03-delay-queue/src/main/java/com/wuhj/listener/OrderFailureListener.java
@@ -1,11 +1,12 @@
 package com.wuhj.listener;
 
-import com.rabbitmq.client.AMQP;
+import com.rabbitmq.client.Channel;
 import com.wuhj.config.RabbitMQConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.Message;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.support.AmqpHeaders;
 import org.springframework.messaging.handler.annotation.Headers;
 import org.springframework.stereotype.Component;
 
@@ -23,15 +24,15 @@ public class OrderFailureListener {
     @RabbitListener(
             queues = RabbitMQConfig.dealQueueOrder //设置订单失效的队列
     )
-    public void process(String order, Message message, @Headers Map<String, Object> headers, AMQP.Channel channel) throws IOException {
+    public void process(String order, Message message, @Headers Map<String, Object> headers, Channel channel) throws IOException {
         
         logger.info("【订单号】 - [{}]",  order);
         // 判断订单是否已经支付，如果支付则；否则，取消订单（逻辑代码省略)
         
         // 手动ack
-//        Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
-        // 手动签收
-//        channel.basicAck(deliveryTag, false);
+        Long deliveryTag = (Long) headers.get(AmqpHeaders.DELIVERY_TAG);
+        //手动签收
+        channel.basicAck(deliveryTag, false);
         System.out.println("执行结束....");
         
     }
